f9daa46adc7c1b550fd2c8f94397f161e8bbe39d
Caleb Phillips <calebrphillips@gmail.com>
2018-08-19 21:13:43 -0400

    Make neural network layers constant

--- neural_network.py
+++ neural_network.py
@@ -3,12 +3,11 @@ from keras.layers import Dense
 
 
 def get_model_generator(input_size):
-    def create_model(hidden_layers=1, hidden_layer_size=input_size, optimizer='rmsprop', init='glorot_uniform'):
+    def create_model(optimizer='adam', init='normal'):
         # create model
         model = Sequential()
-        model.add(Dense(hidden_layer_size, input_dim=input_size, kernel_initializer=init, activation='relu'))
-        for i in range(hidden_layers - 1):
-            model.add(Dense(hidden_layer_size, kernel_initializer=init, activation='relu'))
+        model.add(Dense(12, input_dim=input_size, kernel_initializer=init, activation='relu'))
+        model.add(Dense(4, kernel_initializer=init, activation='relu'))
         model.add(Dense(1, kernel_initializer=init, activation='sigmoid'))
         # Compile model
         model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
