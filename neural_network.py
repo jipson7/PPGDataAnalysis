ddfa07e6dabb45e9e308cf5d403769e572493b9f
Caleb Phillips <calebrphillips@gmail.com>
2018-08-19 21:23:59 -0400

    Remove second hidden layer

--- neural_network.py
+++ neural_network.py
@@ -7,7 +7,6 @@ def get_model_generator(input_size):
         # create model
         model = Sequential()
         model.add(Dense(12, input_dim=input_size, kernel_initializer=init, activation='relu'))
-        model.add(Dense(4, kernel_initializer=init, activation='relu'))
         model.add(Dense(1, kernel_initializer=init, activation='sigmoid'))
         # Compile model
         model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
