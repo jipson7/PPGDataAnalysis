0124506739a9014da5127c8f04e6781697ff2d24
Caleb Phillips <calebrphillips@gmail.com>
2018-09-07 09:43:47 -0400

    Plot algorithmic output

--- visualizations.py
+++ visualizations.py
@@ -1,8 +1,11 @@
 import data
+import pickle
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
+def visualize_algorithms(trial_id, algo_name='enhanced', threshold=1.0):
     devices = data.load_devices(trial_id, algo_name=algo_name)
     wrist = devices[0]
     transitive = devices[2]
@@ -11,8 +14,26 @@ def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
 
     print("Length of Dataframe: " + str(df_length))
 
-    wrist_oxygen = wrist[['oxygen']].values.flatten()
-    true_oxygen = transitive[['oxygen']].values.flatten()
+
+
+    wrist_oxygen = wrist[['oxygen']]
+    true_oxygen = transitive[['oxygen']]
+
+    wrist_oxygen.columns = ['Wrist Oxygen']
+    true_oxygen.columns = ['Fingertip Oxygen']
+
+    graph_df = pd.concat([wrist_oxygen, true_oxygen], axis=1, sort=True)
+
+    assert(wrist_oxygen.shape == true_oxygen.shape)
+    assert(graph_df.shape[0] == wrist_oxygen.shape[0])
+
+    graph_df.plot.line()
+    plt.xlabel("Time")
+    plt.ylabel("SpO2 (%)")
+    plt.show()
+
+    wrist_oxygen = wrist_oxygen.values.flatten()
+    true_oxygen = true_oxygen.values.flatten()
 
     sample_count = wrist_oxygen.shape[0]
     wrist_reliable_count = np.count_nonzero(~np.isnan(wrist_oxygen))
@@ -21,8 +42,6 @@ def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
 
     algo_percent = (wrist_reliable_count / sample_count) * 100
     print("{} algorithm marked {} samples, or {:.1f}%, as reliable".format(algo_name, wrist_reliable_count, algo_percent))
-
-    assert(wrist_oxygen.shape == true_oxygen.shape)
 
     true_reliable_count = 0
     for o1, o2 in zip(wrist_oxygen, true_oxygen):
@@ -34,6 +53,10 @@ def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
     print("{}, or {:.1f}%, of labels were within {} of transitive sensor".format(true_reliable_count, actual_precent, threshold))
 
 
+def visualize_classifier(trial_id, algo_name='enhanced'):
+    clf = pickle.load(open('data-cache/classifier.pickle', "rb"))
+
+
 if __name__ == '__main__':
-    trial_id = 21
+    trial_id = 22
     visualize_algorithms(trial_id, algo_name='enhanced')
