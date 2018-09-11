4a19c13cec33275b10be77c8e1660ef131332d4d
Caleb Phillips <calebrphillips@gmail.com>
2018-09-11 14:37:55 -0400

    Change to wrist device. Updates colours. Absract print stats function

--- visualizations.py
+++ visualizations.py
@@ -7,17 +7,13 @@ import matplotlib.pyplot as plt
 import time
 
 
-def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
+def visualize_algorithms(trial_id, algo_name='enhanced', threshold=1.0):
     devices = data.load_devices(trial_id, algo_name=algo_name)
-    wrist = devices[0]
-    transitive = devices[2]
 
-    df_length = data.get_df_length(wrist)
+    wrist_oxygen = devices[0][['oxygen']]
+    true_oxygen = devices[1][['oxygen']]
 
-    print("Length of Dataframe: " + str(df_length))
-
-    wrist_oxygen = wrist[['oxygen']]
-    true_oxygen = transitive[['oxygen']]
+    print("Length of Dataframe: " + str(data.get_df_length(wrist_oxygen)))
 
     wrist_oxygen.columns = ['Wrist Oxygen']
     true_oxygen.columns = ['Fingertip Oxygen']
@@ -27,14 +23,19 @@ def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
     assert(wrist_oxygen.shape == true_oxygen.shape)
     assert(graph_df.shape[0] == wrist_oxygen.shape[0])
 
-    graph_df.plot.line()
+    graph_df.plot.line(color=['red', 'blue'])
     plt.xlabel("Time")
     plt.ylabel("SpO2 (%)")
     plt.savefig('figs/algo{}.png'.format(time.time()))
     plt.show()
 
-    wrist_oxygen = wrist_oxygen.values.flatten()
-    true_oxygen = true_oxygen.values.flatten()
+    print_stats(wrist_oxygen, true_oxygen, threshold)
+
+
+def print_stats(wrist_device, true_device, threshold):
+
+    wrist_oxygen = wrist_device.values.flatten()
+    true_oxygen = true_device.values.flatten()
 
     sample_count = wrist_oxygen.shape[0]
     wrist_reliable_count = np.count_nonzero(~np.isnan(wrist_oxygen))
@@ -42,7 +43,7 @@ def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
     print("Samples Collected: " + str(sample_count))
 
     algo_percent = (wrist_reliable_count / sample_count) * 100
-    print("{} algorithm marked {} samples, or {:.1f}%, as reliable".format(algo_name, wrist_reliable_count, algo_percent))
+    print("Algorithm marked {} samples, or {:.1f}%, as reliable".format(wrist_reliable_count, algo_percent))
 
     true_reliable_count = 0
     for o1, o2 in zip(wrist_oxygen, true_oxygen):
@@ -51,21 +52,21 @@ def visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0):
             true_reliable_count += 1
 
     actual_precent = (true_reliable_count / sample_count) * 100
-    print("{}, or {:.1f}%, of labels were within {} of transitive sensor".format(true_reliable_count, actual_precent, threshold))
+    print("{}, or {:.1f}%, of labels were within {} of wrist sensor".format(true_reliable_count, actual_precent, threshold))
 
 
 def visualize_classifier(trial_id, algo_name, threshold):
     clf = pickle.load(open('data-cache/classifier.pickle', "rb"))
     devices = data.load_devices(trial_id, algo_name=algo_name)
     wrist = devices[0]
-    transitive = devices[2]
+    truth = devices[1]
     fe = data.FeatureExtractor(window_size=100, threshold=threshold)
     X, y_true = ex.create_training_data([trial_id], fe, algo_name)
     y_pred = clf.predict(X)
 
     # Remove first 99 elements to align with label
     wrist_oxygen = wrist[['oxygen']][99:]
-    true_oxygen = transitive[['oxygen']][99:]
+    true_oxygen = truth[['oxygen']][99:]
     y_pred = y_pred.reshape(wrist_oxygen.shape)
 
     wrist_oxygen_clean = wrist_oxygen.where(y_pred)
@@ -80,17 +81,19 @@ def visualize_classifier(trial_id, algo_name, threshold):
     assert(wrist_oxygen.shape == true_oxygen.shape)
     assert(graph_df.shape[0] == wrist_oxygen.shape[0])
 
-    graph_df.plot.line()
+    graph_df.plot.line(color=['red', 'blue', 'black'])
     plt.xlabel("Time")
     plt.ylabel("SpO2 (%)")
     plt.ylim(ymin=56, ymax=100)
     plt.savefig('figs/clf{}.png'.format(time.time()))
     plt.show()
 
+    print_stats(wrist_oxygen, true_oxygen, threshold)
+
 
 if __name__ == '__main__':
     trial_id = 22
 
-    # visualize_algorithms(trial_id, algo_name='enhanced', threshold=2.0)
+    # visualize_algorithms(trial_id, algo_name='maxim', threshold=1.0)
 
-    visualize_classifier(trial_id, algo_name='enhanced', threshold=3.0)
+    visualize_classifier(trial_id, algo_name='enhanced', threshold=1.0)
