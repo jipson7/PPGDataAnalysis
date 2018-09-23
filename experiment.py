08558585d9ddaa6ebaa232102bf420ada2dcd47d
Caleb Phillips <calebrphillips@gmail.com>
2018-09-23 12:03:15 -0400

    Add rmse calculation and logging to experiment

--- experiment.py
+++ experiment.py
@@ -1,22 +1,33 @@
-import data
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import numpy as np
+import math
+import time
 from sklearn.metrics import confusion_matrix, precision_score
 import xgboost as xgb
+from sklearn.ensemble import GradientBoostingClassifier
 import warnings
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from data import N_JOBS, CM_CACHE
+import data
+from data import N_JOBS, CM_CACHE, EXPERIMENT_CACHE
 
 warnings.filterwarnings(module='sklearn*', action='ignore', category=DeprecationWarning)
 
 
-def create_optimized_classifier(X, y, parameters):
+def optimize_classifier(trial_ids, data_loader):
+    X, y = data_loader.load(trial_ids)
+    parameters = {
+        'learning_rate': [0.1, 0.5, 0.6],
+        'n_estimators': [50, 100, 150],
+        'max_depth': [3, 6]
+    }
     scoring = 'precision'
 
-    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    cv = StratifiedKFold(n_splits=3, shuffle=False)
 
-    clf = GridSearchCV(xgb.XGBClassifier(), param_grid=parameters, scoring=['accuracy', scoring],
-                       cv=cv, verbose=1, refit=scoring, n_jobs=N_JOBS,
+    clf = GridSearchCV(GradientBoostingClassifier(), param_grid=parameters, scoring=['accuracy', scoring],
+                       cv=cv, verbose=2, refit=scoring, n_jobs=N_JOBS,
                        return_train_score=False, iid=False)
     clf.fit(X, y)
     results = clf.cv_results_
@@ -24,39 +35,67 @@ def create_optimized_classifier(X, y, parameters):
     for param, accuracy, score in zip(results['params'], results['mean_test_accuracy'], results['mean_test_' + scoring]):
         print("Accuracy: {:.3f}, {}: {:.3f}, Params: {}".format(accuracy, scoring, score, param))
     print("\nBest {} {}, Params: {}".format(scoring, clf.best_score_, clf.best_params_))
-    return clf.best_estimator_
 
 
-def create_average_cm(clf, trial_ids, data_loader):
+def rmse(d1, d2):
+    d1 = d1.values.flatten()
+    d2 = d2.values.flatten()
+    return math.sqrt(np.nanmean((np.subtract(d2, d1)) ** 2))
+
+
+def run_experiments(clf, trial_ids, data_loader):
+    log_name = EXPERIMENT_CACHE + "{}-{}.txt".format(time.time(), data_loader)
+    log = open(log_name, 'w')
     cms = []
+    precisions = []
+    precisions_weighted = []
     for trial_id in trial_ids:
+        start_msg = "Running experiments on trial " + str(trial_id) + '\n'
+        log.write(start_msg)
+        print(start_msg)
+
+        # Prep leave 1 out data
         training_ids = trial_ids.copy()
         training_ids.remove(trial_id)
-        cm = create_cm(clf, training_ids, trial_id, data_loader)
-        cms.append(cm)
+        X_train, y_train = data_loader.load(training_ids)
+        clf.fit(X_train, y_train)
+        X_test, y_test = data_loader.load([trial_id])
+        y_pred = clf.predict(X_test)
+
+        # Get Precision Scores
+        precisions.append(precision_score(y_test, y_pred))
+        precisions_weighted.append(precision_score(y_test, y_pred, average='weighted'))
+
+        # Create confusion matrix
+        cms.append(confusion_matrix(y_test, y_pred))
+
+        # Get Before and after oxygen values
+        wrist_oxygen, pruned_oxygen, fingertip_oxygen = data_loader.load_oxygen(trial_id, y_pred)
+
+        # Get RMSE
+        rmse_before = rmse(wrist_oxygen, fingertip_oxygen)
+        rmse_after = rmse(pruned_oxygen, fingertip_oxygen)
+        log.write("RMSE Before: {}, After: {}\n".format(rmse_before, rmse_after))
+
+    # Create average confusion matrix
     avg_cm = np.average(cms, axis=0)
     avg_cm = np.array(avg_cm).astype(int)
     data.plot_confusion_matrix(avg_cm)
     plt.savefig(CM_CACHE + 'cm-' + str(data_loader) + '.png')
 
+    log.write("Average Precision: {}".format(np.average(precisions)))
+    log.write("Average Weighted Precision: {}".format(np.average(precisions_weighted)))
 
-def create_cm(clf, training_ids, test_id, data_loader):
-    X_train, y_train = data_loader.load(training_ids)
-    clf.fit(X_train, y_train)
-    X_test, y_test = data_loader.load([test_id])
-    y_pred = clf.predict(X_test)
-    print("Trial {} results: ".format(test_id))
-    print("Precision Weighted: " + str(precision_score(y_test, y_pred, average='weighted')))
-    print("Precision: " + str(precision_score(y_test, y_pred)))
-    return confusion_matrix(y_test, y_pred)
-
+    log.close()
 
 if __name__ == '__main__':
     trial_ids = [22, 23, 24, 29, 31, 32, 33, 36, 40, 43]
 
     clf = xgb.XGBClassifier(n_jobs=N_JOBS)
-    dl = data.DataLoader(window_size=100, threshold=1.0, algo_name='maxim')
+    dl = data.DataLoader(window_size=100, threshold=1.0, algo_name='maxim', features='minimal')
 
-    create_average_cm(clf, trial_ids, dl)
+    run_experiments(clf, trial_ids, dl)
+
+    # optimize_classifier(trial_ids, dl)
 
 
