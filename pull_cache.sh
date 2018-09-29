0ebb8687c83076f3cb5c97077170faff5541dc7a
Caleb Phillips <calebrphillips@gmail.com>
2018-09-29 18:17:45 -0400

    Update for new server

--- pull_cache.sh
+++ pull_cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -ruv --exclude features mel22:/scratch/caleb/ppg/local-cache/* ./local-cache/
+rsync -ruv --exclude features mel23:/mnt/FS1/caleb/ppg/local-cache/* ./local-cache/
--- tunnel.sh
+++ tunnel.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ssh caleb@10.70.20.155 -N -L 5432:localhost:5432
+
