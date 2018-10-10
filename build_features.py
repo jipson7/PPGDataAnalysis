2a59549c4b340b42b6f6c1f879de14f975db527d
Caleb Phillips <calebrphillips@gmail.com>
2018-10-10 07:19:11 -0400

    Add script to build feature pickles with garbage collection

---
+++ build_features.py
@@ -0,0 +1,11 @@
+import trial_sets
+import data
+import gc
+
+
+dl = data.DataLoader(window_size=100, threshold=1.0, algo_name='enhanced', features='comprehensive')
+
+for trial_id in trial_sets.top_ids:
+    print("Building cache for trial " + str(trial_id))
+    X, y = dl.load([trial_id])
+    gc.collect()
